6686030a61bd5b0cb234145eabe32fd29100cd1a
deanbirnie <deanicdl@gmail.com>
2023-05-28 16:11:23 -0700

    Task 1 - Pint opcode

---
+++ pint.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/**
+ * opcode_pint - prints the value at the top of the stack
+ *
+ * @stack: double pointer to the stack
+ * @line_number: line number of the opcode being executed
+ */
+void opcode_pint(stack_t **stack, unsigned int line_number)
+{
+    if (*stack == NULL)
+    {
+        fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("%d\n", (*stack)->n);
+}
