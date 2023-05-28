bacf2d149f9e34bb7d88cfa7ed7dd70569b5899f
deanbirnie <deanicdl@gmail.com>
2023-05-28 16:13:54 -0700

    Main function and header file

---
+++ main.c
@@ -0,0 +1,55 @@
+#include "monty.h"
+
+/**
+ * parse_file - Read and parse the Monty byte code file.
+ *
+ * @filename: Name of the Monty byte code file.
+ */
+void parse_file(const char *filename)
+{
+	FILE *file = fopen(filename, "r");
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	unsigned int line_number = 1;
+
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", filename);
+		exit(EXIT_FAILURE);
+	}
+
+	while ((read = getline(&line, &len, file)) != -1)
+	{
+		if (line[read - 1] == '\n')
+			line[read - 1] = '\0';
+
+		line_number++;
+	}
+
+	free(line);
+	fclose(file);
+}
+
+/**
+ * main - entry point of the program
+ *
+ * @argc: number of arguments in the array
+ * @argv: array of arguments
+ *
+ * Return: exit success or failure
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		return EXIT_FAILURE;
+	}
+
+	const char *filename = argv[1];
+
+	parse_file(filename);
+
+	return EXIT_SUCCESS;
+}
---
+++ monty.h
@@ -0,0 +1,53 @@
+#ifndef _MONTY_H
+#define _MONTY_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+void parse_file(const char *filename);
+
+int main(int argc, char *argv[]);
+
+void opcode_push(stack_t **stack, unsigned int line_number);
+
+void opcode_pall(stack_t **stack, unsigned int line_number);
+
+void opcode_pint(stack_t **stack, unsigned int line_number);
+
+void opcode_pop(stack_t **stack, unsigned int line_number);
+
+void opcode_swap(stack_t **stack, unsigned int line_number);
+
+void opcode_nop(stack_t **stack, unsigned int line_number);
+
+#endif
